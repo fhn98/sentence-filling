0e913dc147b6f265124890553ed98fe79e427f24
fhn98 <42812485+fhn98@users.noreply.github.com>
2018-09-23 19:47:20 +0330

    Add files via upload

--- sentence_filler_attempt1.py
+++ sentence_filler_attempt1.py
@@ -13,7 +13,7 @@ from tensorflow.python.layers.core import Dense
 
 
 class sentence_filler(object):
-    def __init__(self , batch_size  , hidden_len  , num_encoder_layers , num_decoder_layers , vocab_size , keep_prob , embd_len , start_token , end_token , train_mode):
+    def __init__(self , batch_size  , hidden_len  , num_layers , vocab_size , keep_prob , embd_len , start_token , end_token , train_mode):
     """
     description: initializing class fields and then calling construct_model for constructing the model_graph
     
@@ -23,8 +23,7 @@ class sentence_filler(object):
     """
         self.batch_size = batch_size
         self.hidden_len = hidden_len
-        self.num_encoder_layers = num_encoder_layers
-        self.num_decoder_layers = num_decoder_layers
+        self.num_layers = num_layers
         self.vocab_size = vocab_size
         self.keep_prob = keep_prob
         self.embd_len = embd_len
@@ -44,11 +43,13 @@ class sentence_filler(object):
         """
         
         #context words and words of the target sentence.
-        self.context = tf.placeholder(tf.int32 , [self.batch_size ,None])
+        self.before = tf.placeholder(tf.int32 , [self.batch_size ,None])
+        self.after = tf.placeholder(tf.int32 , [self.batch_size ,None])
         self.target = tf.placeholder(tf.int32 , [self.batch_size , None])
         
         #number of words in each row of context tensor and target tensor
-        self.context_length = tf.placeholder(tf.int32 , [self.batch_size])
+        self.before_length = tf.placeholder(tf.int32 , [self.batch_size])
+        self.after_length = tf.placeholder(tf.int32 , [self.batch_size])
         self.target_length = tf.placeholder(tf.int32 , [self.batch_size])
         
         #here we use a placeholder to load the embedding vectors in it with feed_dict
@@ -62,7 +63,9 @@ class sentence_filler(object):
         
         
    
-        context_embedded_weights = tf.nn.embedding_lookup(new_embedding , self.inputs)
+        before_embedded_weights = tf.nn.embedding_lookup(new_embedding , self.before)
+        after_embedded_weights = tf.nn.embedding_lookup(new_embedding , self.after)
+    
     
     
         #basic lstm cell with dropout wrapper. used for encoder
@@ -73,10 +76,26 @@ class sentence_filler(object):
             return cell
         
         #multilayer forward cell used for encoder
-        forward_cell = tf.contrib.rnn.MultiRNNCell([basic_cell() for _ in range (self.num_encoder_layers)])
+        forward_cell = tf.contrib.rnn.MultiRNNCell([basic_cell() for _ in range (self.num_layers)])
+       
+        #passing context words through the encoder. 
+        #the forward cell is unrolled twice. once for words before the blank and once for words after it.
+        #state of the first unrolling is passed as the initial state of the second one.
         
-        #passing context words through the encoder
-        context_output ,_ = tf.nn.dynamic_rnn(cell=forward_cell , inputs= context_embedded_weights , sequence_length=self.context_length)
+        before_output ,before_state = tf.nn.dynamic_rnn(cell=forward_cell , inputs= before_embedded_weights , sequence_length=self.before_length)
+        
+        #making initial state of the second unrolling from last state of first unrolling.
+        state_per_layer_list = tf.unstack(before_state, axis=0)
+        after_init_state = tuple(
+            [tf.contrib.rnn.LSTMStateTuple(state_per_layer_list[idx][0], state_per_layer_list[idx][1])
+             for idx in range(num_layers)]
+        )
+
+        after_output ,_ =  tf.nn.dynamic_rnn(cell=forward_cell , inputs= after_embedded_weights , sequence_length=self.after_length , initial_state=after_init_state)
+        
+        #concatenating before and after outputs to make context_output which is used for attention
+        context_output = tf.concat([before_output , after_output] , axis = 1)
+        context_length=before_length+after_length
         
         #attention on outputs of encoder
         attention_mechanism = tf.contrib.seq2seq.LuongAttention(memory = context_outputs , num_units=self.hidden_len , memory_sequence_length=self.context_length)
@@ -93,7 +112,7 @@ class sentence_filler(object):
             return cell
         
         #making multilayer decoder cell. last layer is attention cell
-        decoder_cell = tf.contrib.rnn.MultiRNNCell([basic_decoder_cell() for _ in range (num_decoder_layers-1)].append(attention_decoder_cell()))
+        decoder_cell = tf.contrib.rnn.MultiRNNCell([basic_decoder_cell() for _ in range (num_layers-1)].append(attention_decoder_cell()))
         
         #training helper
         helper = tf.contrib.seq2seq.TrainingHelper(inputs=self.target ,sequence_length=self.target_length)
@@ -102,7 +121,7 @@ class sentence_filler(object):
         output_logits = Dense (self.vocab_size , use_bias=False) 
         
         #making the decoder
-        decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell , helper=helper , initial_state=prev_state , output_layer=output_logits)
+        decoder = tf.contrib.seq2seq.BasicDecoder(cell=decoder_cell , helper=helper , initial_state=after_initial_state , output_layer=output_logits)
         
         #getting outputs of decoder after unrolling it
         decoder_output,_,_ = tf.contrib.seq2seq.dynamic_decode(decoder,...)
@@ -121,7 +140,7 @@ class sentence_filler(object):
         target_embedding_mean = tf.reduce_mean(tf.multiply(tf.nn.embedding_lookup(embedding , self.target),mask),axis=1)
         
         
-        loss = tf.reduce_mean(tf.square(target_embedding_mean-output_embedding_mean))
+        loss = tf.square(target_embedding_mean-output_embedding_mean)
         
         self.batch_loss = tf.reduce_mean(loss , axis = 0)
         
